1bd970f97b4cc8359ff844ee79de89ba25ec0c5c
jaikrishnaverma-cedcoss <jaikrishnaverma@cedcoss.com>
2023-02-01 18:30:31 +0530

    fourth commit

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ function App() {
         <p>1. Your state preserved untill otp validation not successfully done.</p>
         <p>2. Once you <span className="msg-alert-input-success">complete otp verification</span> Then Modal unmounted and all <span className="msg-alert-input-danger">states & ref free after 1000 ms.</span></p>
         <p>3. For ongoing otp validation click "Finish remaining OTP Verification".</p>
-        {/* <p>4. </p> */}
       </div>
     </>
   );
