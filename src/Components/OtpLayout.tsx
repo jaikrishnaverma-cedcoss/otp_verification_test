a40923800eba0c305de968a01db4c2404435332a
jaikrishnaverma-cedcoss <jaikrishnaverma@cedcoss.com>
2023-02-06 18:28:49 +0530

    commit again

--- src/Components/OtpLayout.tsx
+++ src/Components/OtpLayout.tsx
@@ -1,250 +1,208 @@
-import React, { useEffect, useRef, useState } from "react";
-import {  MyOtpLayoutProps, typeInitialInput } from "../Types/Types";
-const initialInput:typeInitialInput = {
-  digit1: "",
-  digit2: "",
-  digit3: "",
-  digit4: "",
-  digit5: "",
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+import { MyOtpLayoutProps, typeInitialInput } from "../Types/Types";
+type inp = {
+  digit: string[];
+  statusClass: string;
+};
+const initialInput: inp = {
+  digit: [],
   statusClass: "",
 };
-
-export const OtpLayout = ({open, GenerateOtp, currentOtp, closeModal}: MyOtpLayoutProps) => {
+export const OtpLayout = ({
+  GenerateOtp,
+  currentOtp,
+  closeModal,
+}: MyOtpLayoutProps) => {
   const successClass = "alert-input-success";
   const errorClass = "alert-input-danger";
-  const [state, setState] = useState({counter: 5, msg: "", loader:false ,modal:true});
+  const [state, setState] = useState({
+    counter: 4,
+    msg: "",
+    loader: false,
+    modal: true,
+  });
   const [timer, setTimer] = useState<number>(60);
-  const [inputs, setInputs] = useState<typeInitialInput>({...initialInput});
-  const digit1 = useRef<HTMLInputElement>(null);
-  const digit2 = useRef<HTMLInputElement>(null);
-  const digit3 = useRef<HTMLInputElement>(null);
-  const digit4 = useRef<HTMLInputElement>(null);
-  const digit5 = useRef<HTMLInputElement>(null);
+  const [inputs, setInputs] = useState<inp>({ ...initialInput });
 
+  const digit = useRef<HTMLInputElement[]>([]);
+
+  // responsible for resend otp
+  const ResendOtp = () => {
+    if (timer === 0 && state.counter > 0) {
+      if (state.counter >= 0) GenerateOtp();
+      if (state.counter !== -1) state.counter = state.counter - 1;
+      if (state.counter === -1) {
+        setTimer(0);
+      } else setTimer(60);
+      setTimeout(() => {
+        setState({ ...state, msg: "" });
+      }, 1200);
+      setState({ ...state, msg: "One-time passcode sent successfully!" });
+      setInputs({ ...initialInput });
+    }
+  };
+
+  // fill the digit array to empty with the length of otp
+  const filler = () => {
+    inputs.digit = [];
+    currentOtp
+      ?.toString()
+      .split("")
+      .map((x) => {
+        inputs.digit.push("");
+      });
+    setInputs({ ...inputs });
+  };
 
   //  Default focus on first input when component mount
   useEffect(() => {
+    filler();
     setTimeout(() => {
-      digit1.current?.focus();
-    }, 800);
+      digit.current[0]?.focus();
+    }, 100);
   }, []);
 
-
-//   responsible for timer when generateOtp function calls
+  //   responsible for timer when generateOtp function calls
   useEffect(() => {
-    let interval:any  = null;
+    let interval: any = null;
     interval = setInterval(() => {
       setTimer((prev) => (prev == 0 ? 0 : prev - 1));
-      console.log('first')
     }, 1000);
+    filler();
     // callback function for clear interval when component unmount
     return () => {
       clearInterval(interval);
     };
   }, [GenerateOtp]);
 
-
-  // responsible for resend otp
-  const ResendOtp = () => {
-    if(timer===0 && state.counter>0){
-    if (state.counter > 0) GenerateOtp();
-    if (state.counter !== 0) state.counter = state.counter - 1;
-    if(state.counter === 0){
-        setTimer(0)
-    }else
-    setTimer(60)
-    setState({ ...state,msg:'One-time passcode sent successfully!' });
-    setInputs({...initialInput});
-  }
-};
-
-
   // responsible to handle all input digits and focus
-  const InputDigits = (e:any) => {
-    if (!e.target.value) return;
-    let val = e.target.value;
-    if (val.length > 1) val = val.split("")[val.length - 1];
-    switch (e.target.name) {
-      case "digit1":
-        {
-          inputs.digit1 = val;
-          digit2.current?.focus();
-        }
-        break;
-      case "digit2":
-        {
-          inputs.digit2 = val;
-          digit3.current?.focus();
-        }
-        break;
-      case "digit3":
-        {
-          inputs.digit3 = val;
-          digit4.current?.focus();
-        }
-        break;
-      case "digit4":
-        {
-          inputs.digit4 = val;
-          digit5.current?.focus();
-        }
-        break;
-      case "digit5": {
-        inputs.digit5 = val;
+  const InputDigits = (e: ChangeEvent<HTMLInputElement>, i: number) => {
+    if (
+      /^[0-9]{1}$/.test(e.currentTarget.value) ||
+      e.currentTarget.value === ""
+    ) {
+      if (e.currentTarget.value) {
+        digit.current[i + 1]?.focus();
+        if (i < inputs.digit.length - 1) inputs.digit[i + 1] = "";
+      }
+      inputs.digit[i] = e.currentTarget.value;
+      state.msg = "";
+      inputs.statusClass = "";
+      if (inputs.digit.reduce((final, current) => final && current)) {
         // status hold the value of otp matched or not
         let status =
-          inputs.digit1 +
-            inputs.digit2 +
-            inputs.digit3 +
-            inputs.digit4 +
-            val ===
+          inputs.digit.toString().replaceAll(",", "") ===
           currentOtp?.toString();
         inputs.statusClass = status ? successClass : errorClass;
-        digit5.current?.blur()
-        
         // if otp matched
-        if(status){
-            (state.msg = "Enter one time passcode is Correct!");
-             state.loader=true
-            //  after 1000 ms modal will closed automatically
-            setTimeout(()=>{
-              closeModal(false)
-            },1000)
-        }else{
-            (state.msg = "Enter one time passcode is Incorrect!");
+        if (status) {
+          state.msg = "Enter one time passcode is Correct!";
+          state.loader = true;
+          digit.current.forEach((x, index) => {
+            digit.current[index]?.blur();
+          });
+          //  after 1000 ms modal will closed automatically
+          setTimeout(() => {
+            closeModal(false);
+          }, 1000);
+        } else {
+          state.msg = "Enter one time passcode is Incorrect!";
         }
       }
+      setInputs({ ...inputs });
+      setState({ ...state });
     }
-    setInputs({ ...inputs });
-    setState({ ...state });
   };
-
 
   //  to handle input backSpace and focus
-  const handleBackspace = (e: any) => {
-    if (e.key !== "Backspace" || !e.target.value) return;
-    switch (e.target.name) {
-      case "digit1":
-        {
-          inputs.digit1 = "";
-          digit1.current?.focus();
-        }
-        break;
-      case "digit2":
-        {
-          inputs.digit2 = "";
-          digit1.current?.focus();
-        }
-        break;
-      case "digit3":
-        {
-          inputs.digit3 = "";
-          digit2.current?.focus();
-        }
-        break;
-      case "digit4":
-        {
-          inputs.digit4 = "";
-          digit3.current?.focus();
-        }
-        break;
-      case "digit5": {
-        inputs.digit5 = "";
-        digit4.current?.focus();
-      }
-    }
-    setInputs({ ...inputs });
+  const handleBackspace = (e: any, i: number) => {
+    console.log(e.target.value);
+    if (e.key === "Backspace" && !inputs.digit[i])
+      digit.current[i - 1]?.focus();
+    else if (e.key === "ArrowLeft") digit.current[i - 1]?.focus();
+    else if (e.key === "ArrowRight") digit.current[i + 1]?.focus();
+    // else if(e.target.value.length==1 && e.key === "Backspace" )
+    //   digit.current[i-1]?.focus()
   };
-  if(!state.modal){
-    return <button className="btn" onClick={() => setState({...state,modal:true})}>Finish remaining OTP verification</button>
-  }
-  
-return (
+
+  return (
     <>
       <div className={`container modal ${state.modal ? "" : "modal-hidden"}`}>
-
-            <div className="modal-card">
-            <div className="heading">
-              <p >
-                Verify Email Address{" "}
-                <span style={{ margin: "0px 10px" }}>({currentOtp})</span>
-              </p>
-              <button className="btn" onClick={() => setState({...state,modal:false})}>
-                X
-              </button>
-            </div>
-            <div className="body">
-              <p>Enter Your Code Here:</p>
-              <div className="boxes">
-                <input
-                  ref={digit1}
-                  className={`${inputs.statusClass}`}
-                  name="digit1"
-                  onKeyDown={(e) => handleBackspace(e)}
-                  onChange={(e) => InputDigits(e)}
-                  value={inputs.digit1}
-                  type="number"
-                />
-                <input
-                  ref={digit2}
-                  className={`${inputs.statusClass}`}
-                  name="digit2"
-                  onKeyDown={(e) => handleBackspace(e)}
-                  onChange={(e) => InputDigits(e)}
-                  value={inputs.digit2}
-                  type="number"
-                />
-                <input
-                  ref={digit3}
-                  className={`${inputs.statusClass}`}
-                  name="digit3"
-                  onKeyDown={(e) => handleBackspace(e)}
-                  onChange={(e) => InputDigits(e)}
-                  value={inputs.digit3}
-                  type="number"
-                />
-                <input
-                  ref={digit4}
-                  className={`${inputs.statusClass}`}
-                  name="digit4"
-                  onKeyDown={(e) => handleBackspace(e)}
-                  onChange={(e) => {
-                    InputDigits(e);
-                  }}
-                  value={inputs.digit4}
-                  type="number"
-                />
-                <input
-                  ref={digit5}
-                  className={`${inputs.statusClass}`}
-                  name="digit5"
-                  onKeyDown={(e) => handleBackspace(e)}
-                  onChange={(e) => InputDigits(e)}
-                  value={inputs.digit5}
-                  type="number"
-                />
-              </div>
-              <p className={`msg-${inputs.statusClass} notification`} >{state.msg}</p>
+        <div className="modal-card">
+          <div className="heading">
+            <p>
+              Verify Email Address
+              <span>({currentOtp})</span>
+            </p>
+            <button className="btn btn-cross" onClick={() => closeModal(false)}>
+              X
+            </button>
+          </div>
+          <div className="body">
+            <p>Enter Your Code Here:</p>
+            <div className="boxes">
+              {currentOtp
+                ?.toString()
+                .split("")
+                .map((d, i) => (
+                  <input
+                    key={d.toString() + i}
+                    ref={(el: HTMLInputElement) => {
+                      digit.current[i] = el;
+                    }}
+                    className={`${inputs.statusClass}`}
+                    name="digit1"
+                    onChange={(e) => {
+                      InputDigits(e, i);
+                    }}
+                    onKeyDown={(e) => handleBackspace(e, i)}
+                    value={inputs.digit[i]}
+                    type="text"
+                    minLength={1}
+                    maxLength={1}
+                  />
+                ))}
               <div className="spinner-box">
-              {    (state.loader)&&<img className="loader" src="Spinner-1s-200px.gif" alt="loader" />}
+                {state.loader && (
+                  <img
+                    className="loader"
+                    src="Spinner-1s-200px.gif"
+                    alt="loader"
+                  />
+                )}
               </div>
-              <div className="card-details">
-                <p className={`info ${(timer>0 || state.counter===0)&&'disabled'}`} onClick={ResendOtp}>
-                  Resend One-Time Passcode.{" "}
-                  <span className="attempts">
-                    ({state.counter-1} attempts left)
-                  </span>
-                </p>
-                {/* here time formate done while renderning jsx ex: 0:2 => 00:02 */}
-                <p className="timer">{`
-                ${(parseInt((timer / 60).toString())<10)?'0':''}${parseInt((timer / 60).toString())} :
-                ${(parseInt((timer % 60).toString())<10)?'0':''}${timer % 60}`}</p>
-              </div>
+            </div>
+            <p className={`msg-${inputs.statusClass} notification`}>
+              {state.msg}
+            </p>
+
+            <div className="card-details">
+              <p className={`info `} onClick={ResendOtp}>
+                <span
+                  className={`resend ${
+                    (timer > 0 || state.counter === 0) && "disabled"
+                  }`}
+                >
+                  Resend One-Time Passcode.
+                </span>
+                <span className={`attempts ${state.counter == 0 && "warning"}`}>
+                  ({state.counter} attempts left)
+                </span>
+              </p>
+              {/* here time formate done while renderning jsx ex: 0:2 => 00:02 */}
+              <p className="timer">{`
+                ${parseInt((timer / 60).toString()) < 10 ? "0" : ""}${parseInt(
+                (timer / 60).toString()
+              )} :
+                ${parseInt((timer % 60).toString()) < 10 ? "0" : ""}${
+                timer % 60
+              }`}</p>
             </div>
           </div>
-        
-       
+        </div>
       </div>
     </>
   );
 };
+
+export default OtpLayout;
--- src/Components/Register.tsx
+++ src/Components/Register.tsx
@@ -1,34 +1,70 @@
-import React, { useState } from 'react'
-import { OtpLayout } from './OtpLayout'
+import React, { useState } from "react";
+import { OtpLayout } from "./OtpLayout";
 
 const Register = () => {
-    const [modalState,setModalState]=useState<boolean>(false)
-    const [currentOtp,setCurrentOtp]=useState<number|null>(null)
-    
-    // this function is responsible for generate otp 
-    const GenerateOtp=()=>{
-      let otp:number=Math.random()*(99999-10000)+10000
-      otp=parseInt(otp.toString())
-      setCurrentOtp(otp)
-    }
+  const [modalState, setModalState] = useState<boolean>(false);
+  const [otpLength, setOtpLength] = useState(5);
+  const [currentOtp, setCurrentOtp] = useState<number | null>(null);
 
-    // this function is responsible for generate otp and open Modal
-    const modalOpener=()=>{
-        GenerateOtp()
-        setModalState(true)
+  // this function is responsible for generate otp
+  const GenerateOtp = () => {
+    let max=''
+    let min='1'
+    for(let i =1 ; i<=otpLength; i++){
+      max+=9
+      if(i!==1)
+      min+=0
     }
+    let otp: number = Math.random() * (parseInt(max) - parseInt(min)) + parseInt(min);
+    otp = parseInt(otp.toString());
+    setCurrentOtp(otp);
+  };
 
+  // this function is responsible for generate otp and open Modal
+  const modalOpener = () => {
+    GenerateOtp();
+    setModalState(true);
+  };
+
+  // take otp length from user and little-bit secure from inspectMode by regex
+  const takeLength = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    let reg = /^[4-8]{1}$/;
+    let val = e.target.value.toString();
+    if (e.target.value.toString().length > 1)
+      val = e.target.value.toString().substring(e.target.value.length - 1);
+    if (val.match(reg)){
+      setOtpLength(
+        parseInt(val)
+      );
+    }
+  };
+  console.log(otpLength);
   return (
-   <>
-   <button className="btn btn-primary" onClick={modalOpener}>Validate OTP</button>
-    {/* model can itself open and closed with respect to open:boolean */}
-    {/* give conditional rendering just for unmount properly remove all ref and variable memory free */}
-    {
-        (modalState)&&<OtpLayout open={modalState}  GenerateOtp={GenerateOtp}  currentOtp={currentOtp} closeModal={setModalState}/>
-    }
-   
-   </>
-  )
-}
+    <>
+    <h2>Register Component</h2>
+      <label className="">Select Length Of OTP: </label>
+      <select name="take" className="otpLength" defaultValue={5} onChange={takeLength} id="">
+        {
+          [4,5,6,7,8].map(x=> <option value={x}>{x} digit OTP</option>)
+        }
+      </select>
+      <div>
+      <button className="btn btn-primary" onClick={modalOpener}>
+        Validate OTP
+      </button>
+      </div>
+      {/* model can itself open and closed with respect to open:boolean */}
+      {/* give conditional rendering just for unmount properly remove all ref and variable memory free */}
+      {modalState && (
+        <OtpLayout
+          open={modalState}
+          GenerateOtp={GenerateOtp}
+          currentOtp={currentOtp}
+          closeModal={setModalState}
+        />
+      )}
+    </>
+  );
+};
 
-export default Register
+export default Register;
